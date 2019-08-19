b67b32c89809dc3a3441fd105abe6f19dd55c4ea
Naomi <siguneema@gmail.com>
2019-08-19 11:07:40 +0300

    add functionality to forms

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
-export class QuoteFormComponent implements OnInit {
+export class QuoteFormComponent {
+  constructor(){
 
-  constructor() { }
-
-  ngOnInit() {
   }
-
 }
