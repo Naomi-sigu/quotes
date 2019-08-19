79554dfd9287c405fe7922b593bec386477474f9
Naomi <siguneema@gmail.com>
2019-08-19 11:07:04 +0300

    add style to form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteCountPipe } from './quote-count.pipe';
+// import { HighlightDirective } from './highlight.directive';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteCountPipe,
+    // HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
